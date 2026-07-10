f20f007a8484bacc49373a5fced33e57b2491d59
Daniel-J-Kramer <daniel.kramer.j@gmail.com>
2026-07-10 18:33:35 -0500

    made one small change

--- src/main.py
+++ src/main.py
@@ -9,10 +9,10 @@ def main():
         basepath = sys.argv[0]
     else:
         basepath = "/"
-    target_dir = "./docs"
-    source_dir = "./static"
-    content_dir = "./content"
-    template = "./template.html"
+    target_dir = f".{basepath}docs"
+    source_dir = f".{basepath}static"
+    content_dir = f".{basepath}content"
+    template = f".{basepath}template.html"
     if os.path.exists(target_dir):
         shutil.rmtree(target_dir)
     os.mkdir(target_dir)
